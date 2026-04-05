946ce4337228b16585a623098a2dd768932bef5d
k-auaferreira <kaua.fs@outlook.com>
2026-04-05 15:19:23 -0300

    :building_construction: feat: add custom fonts and splash screen handling

--- AppEasyPark/App.tsx
+++ AppEasyPark/App.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { RootNavigator } from './src/navigation';
 import Toast from 'react-native-toast-message';
 
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Thin': require('./assets/fonts/Inter_24pt-Thin.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter_24pt-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter_24pt-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter_24pt-SemiBold.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter_24pt-Bold.ttf'),
+
+    'Montserrat-Thin': require('./assets/fonts/Montserrat-Thin.ttf'),
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
       <NavigationContainer>
